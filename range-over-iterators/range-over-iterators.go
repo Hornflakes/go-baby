bdf4573cf89723a9adb8f7d059aa08dc329c1544
Hornflakes <cristianpetrugug@gmail.com>
2026-05-02 02:13:31 +0300

    feat: mind blown again - the episode when generics and iterators

---
+++ range-over-iterators/range-over-iterators.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"iter"
+	"slices"
+	"strings"
+)
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
+	}
+}
+
+func genFib() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		a, b := 0, 1
+
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
+func main() {
+	lst := List[int]{}
+	lst.Push(3)
+	lst.Push(7)
+	lst.Push(17)
+	lst.Push(23)
+
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
+
+	for part := range strings.SplitSeq("go-baby-go", "-") {
+		fmt.Printf("part: %s\n", part)
+	}
+
+	for n := range genFib() {
+		if n >= 10 {
+			break
+		}
+		fmt.Println(n)
+	}
+}
